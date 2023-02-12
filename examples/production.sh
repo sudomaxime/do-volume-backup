30f896ac7eb90ffab80b71a4066d08c8e3e37ab1
Maxime Nadeau <maxime@agencesudo.ca>
2023-02-12 10:18:45 -0500

    Change image path

--- examples/production.sh
+++ examples/production.sh
@@ -4,7 +4,7 @@ docker run \
 -e DO_SPACE_KEY='YOUR_DO_SPACE_KEY_HERE' \
 -e DO_SPACE_SECRET='YOUR_DO_SPACE_SECRET_HERE' \
 -v YOUR_BACKUP_VOLUME_NAME_HERE:/push \
-docker.pkg.github.com/sudomaxime/do-spaces-docker-volume-backup/backup-tool:latest \
+ghcr.io/sudomaxime/do-volume-backup:latest \
 push --endpoint YOUR_DO_SPACE_ENDPOINT --bucket YOUR_DO_BUCKET_NAME --folder YOUR_DESIRED_BACKUP_NAME; \
 history -d $(history 1)
 
@@ -14,6 +14,6 @@ docker run \
 -e DO_SPACE_KEY='YOUR_DO_SPACE_KEY_HERE' \
 -e DO_SPACE_SECRET='YOUR_DO_SPACE_SECRET_HERE' \
 -v YOUR_BACKUP_VOLUME_NAME_HERE:/pull \
-docker.pkg.github.com/sudomaxime/do-spaces-docker-volume-backup/backup-tool:latest \
+ghcr.io/sudomaxime/do-volume-backup:latest \
 push --endpoint YOUR_DO_SPACE_ENDPOINT --bucket YOUR_DO_BUCKET_NAME --folder YOUR_DESIRED_BACKUP_NAME; \
 history -d $(history 1)
